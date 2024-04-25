0ebb5b5eb838677bcf54acc574a9f7009955cfa8
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-04-25 12:57:33 +0530

    basicvariable.js

--- java script/variable.js
+++
@@ -1,8 +0,0 @@
-let accountEmail = "bhartipriyanshustm@gmail.com"
- var accountPassword = "1234"
-
- accountEmail  = "bharti@gmail.com"
- accountPassword = "123"
- 
- 
- console.log([accountEmail,accountPassword])
