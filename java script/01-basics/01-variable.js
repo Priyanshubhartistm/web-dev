f5cd557b2c6a5515b92227c9a436222622deb303
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-04-25 14:21:03 +0530

    discussion of variable

--- java script/01-basics/01-variable.js
+++ java script/01-basics/01-variable.js
@@ -1,15 +1,24 @@
-const accountId = 123456
-let accountEmail = "bhartistm@gmail.com"
-var accountPassword = "1234"
-accountcity = "delhi"
-accountState = "up"
+
+const accountId = 123456789
+let accountEmail = "bhartipriyanshustm@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
 
 
-accountEmail = "bharti@gmail.com"
-accountPassword = "123"
-accountcity = "patna"
-accountState = "bihar"
+accountEmail = "bharti.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
 
 console.log(accountId);
 
-console.table([accountEmail,accountPassword,accountcity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
