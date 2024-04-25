d0d7fe571c208934b39736a4f1365b5dc798f4ff
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-04-25 14:17:57 +0530

    disscusion of variable

---
+++ java script/01-basics/01-variable.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "bhartistm@gmail.com"
+var accountPassword = "1234"
+accountcity = "delhi"
+accountState = "up"
+
+
+accountEmail = "bharti@gmail.com"
+accountPassword = "123"
+accountcity = "patna"
+accountState = "bihar"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountcity,accountState])
