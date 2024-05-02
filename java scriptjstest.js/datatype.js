7ed82c51d963f4e913cd3292a85243ae522d3e99
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-05-02 22:16:02 +0530

    update

---
+++ java scriptjstest.js/datatype.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") // we are using node js not browser
+
+console.log(3 
+    +
+    3)//code readability should be high
+
+console.log("priyanshu")
+
+
+let name = "priyanshu"
+let age = 20
+let isoLoggedin = false
+
+
+// number data type => 2 to power 53
+// bigint           => large no.only use stock market,treading
+// string           => ""
+// boolean          => true/false
+// null             => standlone value
+// undefined        => 
+// symbol           => unique
+
+
+// object           =>
+
+console.log(typeof "null");
+
+
+
+// 
