504b26531ded441d1af29335f44e22afc8c7d0ec
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-05-29 23:40:14 +0530

    stack

---
+++ javascript_test/stack&heapmemory.js
@@ -0,0 +1,18 @@
+let myyoutubename = "priya.com"
+let anothername = "priyanshubharti"
+anothername = "chai with code"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.com"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
