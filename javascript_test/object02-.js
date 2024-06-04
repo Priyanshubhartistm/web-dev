852f2bb33b81a83145908e5498c477f64c5f9ade
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-04 17:22:09 +0530

    object dissucsion

---
+++ javascript_test/object02-.js
@@ -0,0 +1,24 @@
+//const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "12345"
+tinderUser.name = "priya"
+tinderUser.isloggedin = "false"
+//console.log(tinderUser);
+const regularuser = {
+    email:"bharti@mail.com",
+    fullname: {
+        userfullname: {
+    firstname: "priyanshu",
+    lastname: "bharti"
+   }
+}
+}
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"b"}
+//const obj3 = {obj1,obj2}
+//const obj3 = object.assign({},obj1,obj2,obj4)
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
