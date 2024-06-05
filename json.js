dfac5b9435c7410ededba462482e20c93dff44a6
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-05 10:53:04 +0530

    json give api

---
+++ json.js
@@ -0,0 +1,19 @@
+const course = {
+       coursename: "js in hindi",
+       price: "999",
+       courseInstructor: "hitesh"
+}
+//const {courseInstructor} = course
+//console.log(courseInstructor);
+
+const{courseInstructor:instructor} = course
+console.log(instructor);
+
+//+++++++++++++++++ some basic method for example purpose +++++++++++++++++++++++==//
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "priyanshu")
+// console.log();
