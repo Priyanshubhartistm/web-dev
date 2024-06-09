a6c1fa6ca85f4af5711380cf8e4dedca80535433
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-09 21:13:24 +0530

    break

---
+++ javascript_test/basic04/switch.js
@@ -0,0 +1,105 @@
+// // switch (key) {
+// //     case value:
+        
+// //         break;
+
+// //     default:
+// //         break;
+// // }
+// const month = 3
+// switch (month) {
+//         case 1:
+//             console.log("jan");
+//             break;
+//         case 2:
+//             console.log("feb");
+//             break;
+//         case 3:
+//                 console.log("mar");
+//                  //break;
+//             case 4:
+//                 console.log("april");
+//                  break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
+
+// +++++++++++++++++ for checking (BREAK) value ++++++++++++++++++++++++++++++
+//const month = 2
+
+// switch (month) {
+    
+//     case 1:
+//         console.log("jan");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//        // break;
+//     case 3:
+//         console.log("march");
+        
+//         //break;
+//     case 4:
+//         console.log("april");
+        
+//         //break;
+//         default:
+//             console.log("default value match");
+        
+//         break;
+// }
+
+
+// +++++++++++++++++++++++++++++ for checking (STRING VALUE ) YA ANOTHER DATA TYPE+++++++++++++++++++
+
+// const month = "march"
+
+// switch (month) {
+//        case "jan":
+//            console.log(1);
+//            break;
+//         case "feb" :
+//             console.log(2);
+//             break;
+//         case "march":
+//             console.log(3);
+//             //break;
+//         case "april":
+//             console.log(4);
+//             //break;
+//     default:
+//         console.log("default value match");
+//         break;
+// }
+
+
+//+++++++++++++++++++++++++++= another value checking +++++++++++++++++
+
+const weak = "wednesday"
+
+switch (weak) {
+    case "monday":
+        console.log(1);
+        
+        break;
+    case "tuesday":
+        console.log(2);
+        
+        break;
+    case "wednesday":
+        console.log(3);
+        
+        //break;  +++++++++++ (BREAK) me comment lagane ,ya break ni likhne... se ek step aage ka print ya execute hoga..
+    case "thursday":
+        console.log(4);
+        
+        
+
+    default:
+        console.log(2);
+        break;
+}
