5b0d2cad63d8870bae004e6216000049dd3defcc
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-12 13:34:25 +0530

    truth.js

--- javascript/truth.js
+++ javascript/truth.js
@@ -13,15 +13,29 @@ if (userEmail) {
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
 
+
+// +++++++++++++++++++ check empty array ++++++++++++++
+// const useremail = []
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
 
+//+++++++++++++++++ check empty object +++++++++++++++++++
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+// +++++++++++++++ some most important truthy values ++++++++++++++
+// false ==0
+//true
+
+// false ==''
+//true
+
+// 0 == ''
+//true
 
 // Nullish Coalescing Operator (??): null undefined
 
