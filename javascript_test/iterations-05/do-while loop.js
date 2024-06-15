2b3a34457500847a537abd86ae331f97c4ac2f98
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-15 21:59:58 +0530

    do while loop

---
+++ javascript_test/iterations-05/do-while loop.js
@@ -0,0 +1,16 @@
+//+++++++++++++++++++++++ (do means kam phle) DO-WHILE LOOP(kam pahle condition bad me check hota h..) +++++++++++++++++
+// let score = 1
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10)
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
+// ++++++++++ o/p:- 11 isliya bcz, kam pahle condition check bad me
