8534b71c750baf119f9fee57cc03c41ace9bfa9f
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-15 21:54:31 +0530

    while loop

---
+++ javascript_test/iterations-05/whileloop.js
@@ -0,0 +1,26 @@
+// while loop :- Same as it is for loop but, structure are different.
+
+//while (condition) {
+    
+//}
+
+
+// let index =0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+    
+//     index = index+2
+// }
+
+
+
+//++++++++++++++++ WHILE LOOP IN ARRAY ++++++++++++++++
+
+let myArray = ["priya", "bharti","pariyanshu", "anshu"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
