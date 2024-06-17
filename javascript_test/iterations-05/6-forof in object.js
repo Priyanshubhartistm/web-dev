c4027666a41235f3387668d5bd53e4d2f5d04f8e
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-06-17 10:37:38 +0530

    for of loop dissusion

---
+++ javascript_test/iterations-05/6-forof in object.js
@@ -0,0 +1,56 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+
+
+
+
+// ++++++++++++++++++++++ keys aa gyi ,but (OBJECT VALUES ) to uske liye
+
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+
+
+
+// ++++++++++++++++++++++= myObject[key] .. agr isko run krna ho to +++++++++++++++++
+
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+
+
+// ++++++++++++++++++++ FOR- IN LOOP iN ARRAY ++++++++++++++
+
+const programming = ["js","rb","cpp"]
+
+for (const key in programming) {
+    console.log(key);
+}
+
+//+++++++++++++++++++++++ how to declare (programming) value+++++++++++++++++++++++++++++
+
+const pro
