03b9e0060d4e6647bddc5bb620a9c19e563760ee
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-07-18 13:03:00 +0530

    set

---
+++ javascript_test/10- classess and oop/11get/set.js
@@ -0,0 +1,39 @@
+// class user {
+//     constructor(email,password){
+//         this.email;
+//         this.password
+//     }
+//     get password(){
+//         return this._password.toUpperCase()
+//     }
+//     set password(value){
+//         this._password = value
+//     }
+// }
+// const priyanshu = new user("p@mail.com","123")
+// console.log(priyanshu.password);
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    // get email(){
+    //     return this._email.toUpperCase()
+    // }
+    // set email(value){
+    //     this._email = value
+    // }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "123")
+console.log(hitesh.email);
