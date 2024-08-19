55b6eeb43862d63bcc7d0fd69467c8fb01b0b741
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-19 20:43:44 +0530

    var

---
+++ javascript_test/basic01/01-variable.js.js
@@ -0,0 +1,16 @@
+const accountID = 123456
+let accountEmail = "bhRTI@gail.com"
+var accountpassword = "1111"
+accountCity = "jaipur"
+let accountstate;
+
+//accountID = 2 // not allowed
+
+accountEmail ="priya@gmail.com"
+accountpassword = "555"
+accountCity = "patna"
+
+console.log(accountID);
+
+
+console.table([accountID,accountEmail,accountpassword,accountCity,accountstate])
