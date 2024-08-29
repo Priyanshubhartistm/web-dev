b21ba328313d35d9e02e04540216780e7bb43209
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2024-08-29 08:26:25 +0530

    conversion

---
+++ javascript_test/basic01/conversion.js
@@ -0,0 +1,48 @@
+ let score ="6abc"
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+ //console.log(score);
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+ // 33 => 33
+ // "33abc" => NaN (NOT A NUMBER)
+ // true => 1; false=> 0
+
+let isLoggedIn = "priya"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(1)
+//console.log(typeof 1)
+//console.log(booleanIsLoggedIn);
+ 
+let someNumber = 33
+ let stringNumber = String(someNumber)
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+
+
+ */ operator*/
+
+ let value = 3
+ let negValue = -value
+ let positveValue = value
+ //console.log(negValue);
+ //console.log(positveValue);
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);
+ console.log(2/2);
+ console.log(2%2);
+
+ //let str1 =  'HELLO'
+ //let str2 = "priya"
+ //let str3 = str1+str2
+
+ //console.log(str1+str2);
+
+ 
